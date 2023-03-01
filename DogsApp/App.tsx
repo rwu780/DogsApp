193fe164e2d916cca4555cf3fec02616da02febb
James <rwu780@gmail.com>
2023-03-01 10:56:36 -0800

    Init project setup

---
+++ DogsApp/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native'
+
+
+
+
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Abcde</Text>
+    </View>
+
+  );
+}
+
+
+
+export default App;
