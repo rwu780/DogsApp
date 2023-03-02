a5ceeb3897d1d93dd0f8d0be1c596622f29a2af6
James <rwu780@gmail.com>
2023-03-01 19:52:32 -0800

    Project completed

--- DogsApp/App.tsx
+++ DogsApp/App.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native'
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
+import HomeScreen from './Screens/HomeScreen';
+import ImageScreen from './Screens/ImageScreen';
 
-
+const Stack = createStackNavigator();
 
 function App(): JSX.Element {
-  return (
-    <View>
-      <Text>Abcde</Text>
-    </View>
 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Image" component={ImageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 
 
 export default App;
